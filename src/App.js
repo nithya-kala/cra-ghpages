e1568b6b3b6c4b1703d539dc0c642dfaeb9a036b
nithya-kala <nithyakala1988@gmail.com>
2022-07-13 18:00:59 +0530

    Manual Deployment verification

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Deploy CRA App with Github Actions
+          Deploy CRA App with GitHub Actions
         </p>
         <a
           className="App-link"
